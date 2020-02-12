e7d84ce900b4fe5336ba180b6f2e293de0b6696f
mysterybiscuit <guest91111@yandex.com>
2020-02-12 11:53:22 +0100

    Added capacity to fetch user input

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import * as ELEMENTS from "./elements";
 
 const searchWeather = () => {
-  console.log("Clicked!");
+  const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
+  if (CITY_NAME.length == 0) {
+    return alert("Please enter a city name!");
+  }
 };
 
 ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener("click", searchWeather);
