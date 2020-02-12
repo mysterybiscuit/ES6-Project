f7005632d1f0568da0c5c5aedffc52126814797a
mysterybiscuit <guest91111@yandex.com>
2020-02-12 11:30:56 +0100

    Fetching all elements.

---
+++ src/elements.js
@@ -0,0 +1,11 @@
+export const ELEMENT_SEARCH_BUTTON = document.querySelector("button");
+export const ELEMENT_SEARCHED_CITY = document.querySelector("#city");
+export const ELEMENT_LOADING_TEXT = document.querySelector("#load");
+export const ELEMENT_WEATHER_BOX = document.querySelector("#weather");
+export const ELEMENT_WEAHTER_CITY = document.querySelector("#weatherCity");
+export const ELEMENT_WEATHER_DESCRIPTION = document.querySelector(
+  "#weatherDescription"
+);
+export const ELEMENT_WEATHER_TEMPERATURE = document.querySelector(
+  "#weatherTemperature"
+);
