23c2c4b9703054fd3812a8cc2f4abf60d6f11bf6
mysterybiscuit <guest91111@yandex.com>
2020-02-12 12:15:36 +0100

    HTTP & Promises

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as ELEMENTS from "./elements";
 
 const searchWeather = () => {
   const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
-  if (CITY_NAME.length == 0) {
+  if (CITY_NAME.length === 0) {
     return alert("Please enter a city name!");
   }
 };
